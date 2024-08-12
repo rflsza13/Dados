ed8788c0c8068215c748cb5b6ee8084a2d6256c0
UrusianX <rafaelrodriguessouza10@gmail.com>
2024-08-12 13:10:04 -0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
 sessionStorage.setItem("user1", "rafael");
-sessionStorage.setItem("senha1", "153690");
+sessionStorage.setItem("senha1", "10082024");
 sessionStorage.setItem("user2", "julia");
-sessionStorage.setItem("senha2", "284600");
+sessionStorage.setItem("senha2", "10082024");
