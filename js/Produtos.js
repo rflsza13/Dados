d5bc775b802967ee57366909844a288c621555f1
Rafael Souza <rafaelrodriguessouza10@gmail.com>
2025-03-04 17:29:11 -0300

    Criação produtos.js e Alteração data.js

---
+++ js/Produtos.js
@@ -0,0 +1,33 @@
+// Produtos em Prata
+const prdPrataMasc = ["Pulseira Trança Baiana", "Anel trançado"];
+const vlrPrataMasc = ["R$190", "R$75"];
+const prdPrataFem = []
+const vlrPrataFem = [];
+
+// Produtos em Semi-Joias
+const prdJoiasMasc = [];
+const vlrJoiasMasc = [];
+const prdJoiasFem = [];
+const vlrJoiasFem = [];
+
+// Acessórios
+const prdAcessMasc = [];
+const vlrAcessMasc = [];
+const prdAcessFem = [];
+const vlrAcessFem = [];
+
+//Exportação
+sessionStorage.setItem("prdPrataMasc", prdPrataMasc);
+sessionStorage.setItem("vlrPrataMasc", vlrPrataMasc);
+sessionStorage.setItem("prdPrataFem", prdPrataFem);
+sessionStorage.setItem("vlrPrataFem", vlrPrataFem);
+
+sessionStorage.setItem("prdJoiasMasc", prdJoiasMasc);
+sessionStorage.setItem("vlrJoiasMasc", vlrJoiasMasc);
+sessionStorage.setItem("prdJoiasFem", prdJoiasFem);
+sessionStorage.setItem("vlrJoiasFem", vlrJoiasFem);
+
+sessionStorage.setItem("prdAcessMasc", prdAcessMasc);
+sessionStorage.setItem("vlrAcessMasc", vlrAcessMasc);
+sessionStorage.setItem("prdAcessFem", prdAcessFem);
+sessionStorage.setItem("prdAcessFem", prdAcessFem);
--- js/data.js
+++ js/data.js
@@ -1,4 +1,10 @@
-sessionStorage.setItem("user1", "rafael");
-sessionStorage.setItem("senha1", "10082024");
-sessionStorage.setItem("user2", "julia");
-sessionStorage.setItem("senha2", "10082024");
+const user = ["rafael", "julia"];
+const password = ["10082024", "10082024"]
+
+sessionStorage.setItem("user", user);
+sessionStorage.setItem("senha", password);
+
+// sessionStorage.setItem("user1", "rafael");
+// sessionStorage.setItem("senha1", "10082024");
+// sessionStorage.setItem("user2", "julia");
+// sessionStorage.setItem("senha2", "10082024");
