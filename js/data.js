731a38f80f3d8baab9174201a6c60b390f16afbd
Rafael Souza <rafaelrodriguessouza10@gmail.com>
2025-03-05 15:28:31 -0300

    Alteração data.js

--- js/data.js
+++ js/data.js
@@ -1,10 +1,12 @@
-const user = ["rafael", "julia"];
-const password = ["10082024", "10082024"]
+const user = [
+    {
+        usuario: "rafael",
+        senha: "10082024"
+    },
+    {
+        usuario: "julia",
+        senha: "10082024"
+    }
+]
 
-sessionStorage.setItem("user", user);
-sessionStorage.setItem("senha", password);
-
-// sessionStorage.setItem("user1", "rafael");
-// sessionStorage.setItem("senha1", "10082024");
-// sessionStorage.setItem("user2", "julia");
-// sessionStorage.setItem("senha2", "10082024");
+sessionStorage.setItem("user", JSON.stringify(user));
